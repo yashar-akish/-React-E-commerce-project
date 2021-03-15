aafecd93302b489e0fba1aac9c6966aa1b8152a7
yashar-akish <yashar_akish@yahoo.com>
2021-03-15 13:57:28 -0400

    installing redux-persist to store cart content in local storage

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,6 +3,8 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+
+//step: 1 >>>
 const config = {
   apiKey: "AIzaSyAqhcm7dxkR2Fr54Q5hFYXDZj485a1-13s",
   authDomain: "crwn-db-acc58.firebaseapp.com",
@@ -13,13 +15,14 @@ const config = {
   measurementId: "G-D02CP3TLKP"
 };
 
+// saving from google authentication to database
 export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
   const snapShot = await userRef.get();
-
+  //creating new user in db if it is not in db
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
@@ -36,14 +39,18 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
   return userRef;
 }
-
+//step: 2 >>>
 firebase.initializeApp(config);
-
+//step: 3 >>>
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+//step: 4 >>>  setting up the google authentication utility 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+
+
 
 export default firebase;
